8717e15e2485166d6030700e178f5938efbf2d96
Rishav <rishavkapoor38@gmail.com>
2023-04-16 23:05:42 +0530

    initial connit[D[D[D[C[C[C[C

---
+++ js1.js
@@ -0,0 +1,8 @@
+console.log("Hello this is the javascript file.");
+const people=["Rishav","Raj","Kishan","Mohan","Ramesh"];
+console.log(people);
+const first = people.shift();
+people.unshift("Shubham");
+const now=people[1];
+console.log(now);
+console.log(people);
